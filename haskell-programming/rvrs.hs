b08229b20030d2299bd43eadca4d8e351076e18c
Fernando <ffmp777@yahoo.com>
2018-03-09 05:55:42 -0700

    changed rvrs file a bit

--- haskell-programming/rvrs.hs
+++ haskell-programming/rvrs.hs
@@ -1,7 +1,10 @@
 module Rvrs where
 
 rvrs :: String -> String
-rvrs s = concat [third, " ", second, " ", first]
-  where first = take 5 s
-        second = take 2 $ drop 6 s
-        third = drop 9 s
+rvrs s = concat [e, " ", m, " ", b]
+  where b = take 5 s
+        m = take 2 $ drop 6 s
+        e = drop 9 s
+
+main :: IO()
+main = print()
