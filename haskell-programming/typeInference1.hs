543fa06fa8089e23f683349fb07a1e79c028ef61
Fernando <ffmp777@yahoo.com>
2018-03-27 07:10:09 -0600

    notes: type inference

---
+++ haskell-programming/typeInference1.hs
@@ -0,0 +1,17 @@
+module TypeInference1 where
+
+f:: Num a => a -> a -> a
+
+f x y = x + y + 3
+
+-- Prelude> :l typeInference1.hs
+-- [1 of 1] Compiling TypeInference1   ( typeInference1.hs, interpreted )
+-- Ok, one module loaded.
+-- *TypeInference1> f 1 2
+-- 6
+-- *TypeInference1> :t f
+-- f :: Num a => a -> a -> a
+-- *TypeInference1> :t f 1
+-- f 1 :: Num a => a -> a
+-- *TypeInference1>
+-- cant get a more specific type when applying f to 1, because of numeric literals in Haskell are a polymorphic type Num a => a.
