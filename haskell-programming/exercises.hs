ef013b35869688d7cfde59dd10e985bb07ff89e4
Fernando <ffmp777@yahoo.com>
2018-05-11 06:59:39 -0600

    exercises: ch 7.6

--- haskell-programming/exercises.hs
+++ haskell-programming/exercises.hs
@@ -343,3 +343,22 @@ probNumThree = z / x + y
 -- 2. 
 -- f :: (a, b, c) -> (d, e, f) -> ((a, d), (c, f))
 -- f (a, b, c) (d, e, f) = ((a, d), (c, f)) ??
+
+-- 7.5
+-- Case Practice
+-- 1. function x y = if (x > y) then x else y
+-- function x y =
+--     case (x > y) of
+--         True -> x
+--         False -> y
+-- 2. ifEvenAdd2 n = if even n then (n+2) else n
+-- ifEvenAdd2 n =
+--     case isEven of
+--         True -> (n + 2)
+--         False -> n
+--     where isEven = (even n)
+-- 3. nums x = 
+--     case compare x 0 of
+--         LT -> -1
+--         GT -> 1
+--         EQ -> 0
