c2f5de2504fb34e6cfa1a577107f518ad1f972b1
Fernando <ffmp777@yahoo.com>
2018-11-14 06:18:16 -0700

    ch 10 exercise

--- haskell-programming/time.hs
+++ haskell-programming/time.hs
@@ -14,3 +14,38 @@ theDatabase =
             (fromGregorian 1921 5 1)
             (secondsToDiffTime 34123))
     ]
+
+-- 1.
+
+filterDbDate :: [DatabaseItem] -> [UTCTime]
+filterDbDate = foldr getTimes []
+  where
+    getTimes :: DatabaseItem -> [UTCTime] -> [UTCTime]
+    getTimes (DbDate time) dates = time:dates
+    getTimes _ dates = dates
+
+-- 2.
+
+filterDbNumber :: [DatabaseItem] -> [Integer]
+filterDbNumber = foldr getNums []
+  where
+    getNums :: DatabaseItem -> [Integer] -> [Integer]
+    getNums (DbNumber n) ns = n:ns
+    getNums _ ns = ns
+
+-- 3.
+
+mostRecent :: [DatabaseItem] -> UTCTime
+mostRecent = maximum . filterDbDate
+
+-- 4.
+
+sumDb :: [DatabaseItem] -> Integer
+sumDb = sum . filterDbNumber
+
+-- 5.
+
+avgDb :: [DatabaseItem] -> Double
+avgDb db = total / entries
+  where total = (fromIntegral . sumDb) db
+        entries = (fromIntegral . length . filterDbNumber) db
